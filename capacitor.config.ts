9d1cec55ccd12c468bc1b67c7f9704177d799006
Civic Intel Dev <dev@civicintel.app>
2026-06-13 19:25:59 +0300

    Fix webDir to dist where index.html actually is

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.yourapp.id',
   appName: 'Civic Intel',
-  webDir: 'dist/client'
+  webDir: 'dist'
 };
 
 export default config;
